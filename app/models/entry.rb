bf9f40865cde116b7f02826e2cdf4b538cfa258d
Shankarshan Mishra <gautamshan1102@gmail.com>
2023-06-07 13:50:59 +0530

    Calories count

---
+++ app/models/entry.rb
@@ -0,0 +1,8 @@
+class Entry < ApplicationRecord
+
+    validates :meal_type, :calories, :proteins, :carbohydrates, :fats,  presence: true
+
+    def day
+        self.created_at.strftime("%b %e, %Y")
+    end
+end
